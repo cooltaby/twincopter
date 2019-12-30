a6abb9735a327c51bef66df61fa71f7d5fd3b34a
cooltaby <cooltaby@users.noreply.github.com>
2019-12-30 17:03:03 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1577705593,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/arrowup1-sheet0.png",
+		"images/blockbottom-sheet0.png",
+		"images/destroyer-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/helicopter1-sheet0.png",
+		"images/helicopter2-sheet0.png",
+		"images/twincopter-sheet0.png",
+		"images/hudbest-sheet0.png",
+		"images/hudscore-sheet0.png",
+		"media/score.m4a",
+		"media/score.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
